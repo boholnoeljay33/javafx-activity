c426d293d9e463d35a18a87ae58bda1d232a1164
JanGlaver6 <lastidorm@gmail.com>
2025-04-15 14:19:16 +0800

    programming activity

--- src/main/java/aclcbukidnon/com/javafxactivity/controllers/CounterController.java
+++ src/main/java/aclcbukidnon/com/javafxactivity/controllers/CounterController.java
@@ -1,4 +1,41 @@
 package aclcbukidnon.com.javafxactivity.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
 public class CounterController {
+
+    @FXML
+    private Button decrementButton;
+
+    @FXML
+    private Button incrementButton;
+
+    @FXML
+    private Label counterLabel;
+
+    private int counter = 0; // Encapsulation: private counter variable
+
+    @FXML
+    private void initialize() {
+        updateCounterLabel();
+
+        decrementButton.setOnAction(_ -> decrement());
+        incrementButton.setOnAction(_ -> increment());
+    }
+
+    private void increment() {
+        counter++;
+        updateCounterLabel();
+    }
+
+    private void decrement() {
+        counter--;
+        updateCounterLabel();
+    }
+
+    private void updateCounterLabel() {
+        counterLabel.setText(String.valueOf(counter));
+    }
 }
